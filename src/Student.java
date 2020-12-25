f8039af58e3b7c9a64b4ff945552ddbd57bb5949
nguyenhuyhoanlc <nguyenhuyhoanlc@gmail.com>
2020-12-25 09:05:16 +0700

    Thuc hanh

---
+++ src/Student.java
@@ -0,0 +1,35 @@
+public class Student {
+    private String name;
+    private int age;
+    private String address;
+
+    public Student(){
+    }
+
+    public Student(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
